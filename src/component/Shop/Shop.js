3666ddb1078a5ea57d7cfd455be96c51e9a261fb
smfahiim25 <smfahimhossen@gmail.com>
2022-03-25 18:53:55 +0600

    code added

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                {
+                    products.map(product => <Product product={product} key={product.id}></Product>)
+                }
+            </div>
+            <div className='cart-container'>
+                <h3>order container</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
