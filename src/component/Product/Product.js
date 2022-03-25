1dc91534d055859e152b54c42af2a0a85ada607c
smfahiim25 <smfahimhossen@gmail.com>
2022-03-26 00:31:55 +0600

    cart complete

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -13,7 +13,7 @@ const Product = (props) => {
                 <p className='product-name'>{name}</p>
                 <p>Price: ${price}</p>
                 <p><small>Seller: {seller}</small></p>
-                <p><small>Ratings: {ratings}stars</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
             </div>
             <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
                 <p className='btn-text'>Add to cart</p>
